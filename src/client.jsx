7a89b636bf644404f0f8c0bb6368820ed0883dc6
indra2685 <lnursej@gmail.com>
2026-02-01 23:49:15 -0500

    updated syntax error in client.jsx

--- src/client.jsx
+++ src/client.jsx
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = https://anamvmglakgokcdqcgtn.supabase.co
-const API_KEY = sb_publishable_mWCJcKAq_9VN3ystau5Fnw_wyKRGxEt;
-
+const URL = "https://anamvmglakgokcdqcgtn.supabase.co";
+const API_KEY = "sb_publishable_mWCJcKAq_9VN3ystau5Fnw_wyKRGxEt";
 
 export const supabase = createClient(URL, API_KEY);
